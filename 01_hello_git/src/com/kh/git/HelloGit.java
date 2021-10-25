7b8a7a0771561acbe75e16d1b22693ea5b018d50
hyunha95 <76119021+hyunha95@users.noreply.github.com>
2021-10-25 21:24:32 +0900

    땡겨 메세지 추가

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -14,5 +14,6 @@ public class HelloGit {
 		System.out.println(333);
 		System.out.println("이제 github repo에서 관리합니다.");
 		System.out.println("원격 저장소에서 작성!!!!");
+		System.out.println("한번에 땡겨주세요!!!!");
 	}
 }
